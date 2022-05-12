7a4b53641f6b6e716011c446bf2beb871bda64f6
Marsel <63933205+iammarsel@users.noreply.github.com>
2022-05-12 14:45:57 -0400

    size and text

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tkinter as tk
-window = tk.Tk()
+from tkinter import *
+
+window = Tk()
+window.geometry('500x500')
 window.title("Determinate Beam Calculator")
+window.resizable(False,False)
+title = Label(window,text="Hello")
+
+title.grid(row=0,column=0)
 window.mainloop()
 # testing
