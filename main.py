dcf8de589df4e26830a7539560165e693a051374
aleemwi <waleed.aleem@yahoo.com>
2022-05-12 15:32:29 -0400

    units

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from tkinter import *
 window = Tk()
 window.geometry('500x500')
 window.title("Determinate Beam Calculator")
-window.resizable(False,False)
+window.resizable(True,True)
 title = Label(window,text="Hello")
-
+inputtxt = Text(window, height = 2, width = 10)
+inputtxt.grid(row=1,column=0)
+clicked = " "
+options = ["in", "ft", "m", "mm",]
+drop = OptionMenu(window , clicked , *options )
+drop.grid(row=1,column=1)
 title.grid(row=0,column=0)
 window.mainloop()
 # testing
