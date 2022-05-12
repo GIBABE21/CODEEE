d3ff704419393221b4e7ce8ba438ab0244850dc9
Marsel <63933205+iammarsel@users.noreply.github.com>
2022-05-12 15:31:45 -0400

    class for graph

--- main.py
+++ main.py
@@ -3,9 +3,34 @@ from tkinter import *
 window = Tk()
 window.geometry('500x500')
 window.title("Determinate Beam Calculator")
-window.resizable(False,False)
+window.resizable(True,True)
 title = Label(window,text="Hello")
 
 title.grid(row=0,column=0)
 window.mainloop()
 # testing
+
+graph1 = Graph()
+graph1.add_force(2,5)
+
+
+class Graph():
+  def __init__(self):
+      self.seperators = []   # |____|____|  [ 0, 0.4,0.8,2.2,4.7,10]
+      self.forces = {0.4:6} # location:value
+      self.length = 0
+      self.moments = {} # location:values
+  def add_force(self,value,location):
+    if location in self.forces:
+      self.forces[location] += value
+    else:
+      self.forces[location] = value
+      self.seperators.append(location)
+  def add_moment(self,value,location):
+    if location in self.forces:
+      self.moments[location] += value
+    else:
+      self.moments[location] = value
+      self.seperators.append(location)
+  def update_forces(self):
+    print('do it here')
