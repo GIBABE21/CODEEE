f9425cff46a206722e594d4d84ee3ca73fac6105
Marsel <63933205+iammarsel@users.noreply.github.com>
2022-05-13 00:41:57 -0400

    First Graph Test + sorted working (hopefully)

--- main.py
+++ main.py
@@ -1,45 +1,5 @@
 from tkinter import *
-
-# Python program for implementation of heap Sort
-  
-# To heapify subtree rooted at index i.
-# n is size of heap
-def heapify(arr, n, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1     # left = 2*i + 1
-    r = 2 * i + 2     # right = 2*i + 2
-  
-    # See if left child of root exists and is
-    # greater than root
-    if l < n and arr[i] < arr[l]:
-        largest = l
-  
-    # See if right child of root exists and is
-    # greater than root
-    if r < n and arr[largest] < arr[r]:
-        largest = r
-  
-    # Change root, if needed
-    if largest != i:
-        arr[i],arr[largest] = arr[largest],arr[i]  # swap
-  
-        # Heapify the root.
-        heapify(arr, n, largest)
-  
-# The main function to sort an array of given size
-def heapSort(arr):
-    n = len(arr)
-  
-    # Build a maxheap.
-    # Since last parent will be at ((n//2)-1) we can start at that location.
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-  
-    # One by one extract elements
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]   # swap
-        heapify(arr, i, 0)
-  
+import matplotlib.pyplot as plt
 
 class Graph():
   def __init__(self, length, begin, end):
@@ -65,9 +25,8 @@ class Graph():
       self.moments[location] = value
       self.seperators.append(location)
   def update_forces(self):
-    self.seperators = heapSort(self.seperators)
-    print(self.seperators)
-    for i in heapSort(self.seperators):
+    #print(self.seperators)
+    for i in self.seperators:
         if i == self.begin:
             ay = 0
             total = 0
@@ -100,29 +59,65 @@ class Graph():
             else:
                 self.outputs.append(self.forces[i] + self.outputs[-1])
 
-window = Tk()
-window.geometry('500x500')
-window.title("Determinate Beam Calculator")
-window.resizable(True,True)
-title = Label(window,text="Hello")
-inputtxt = Text(window, height = 2, width = 10)
-inputtxt.grid(row=1,column=0)
-clicked = " "
-options = ["in", "ft", "m", "mm",]
-drop = OptionMenu(window , clicked , *options )
-drop.grid(row=1,column=1)
-title.grid(row=0,column=0)
 
-
-
-def task():
+def openGraph():
     graph1 = Graph(10, 2, 6)
-    #graph1.length = 10
+        #graph1.length = 10
     graph1.add_force(1,2)
     graph1.add_force(1.5,3)
     graph1.add_force(4.5,4)
+    graph1.seperators = sorted(graph1.seperators)
     graph1.update_forces()
-    print(graph1.outputs)
+    global inputtxt
+    global left_input
+    global right_input
+    print(value_inside.get())
+    try:
+      print(float(left_input.get()),float(right_input.get()),float(inputtxt.get()))
+    except:
+      print("Invalid Inputs") 
+
+    x = graph1.seperators #graph
+      # corresponding y axis values
+    y = graph1.outputs
+    # plotting the points 
+    plt.plot(x, y)
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+    # giving a title to my graph
+    plt.title('Beam Display')
+    # function to show the plot
+    plt.show()
+
+window = Tk()
+window.geometry('500x500')
+window.title("Determinate Beam Calculator")
+#window.state('zoomed')
+window.resizable(True,True)
+
+title = Label(window,text="Beam Numbers").grid(row=0,column=2)
+inputtxt = Entry(window, width = 10)
+inputtxt.grid(row=1,column=2)
+left_input = Entry(window, width = 10)#
+left_input.grid(row=4,column=2)
+right_input = Entry(window, width = 10)#
+right_input.grid(row=4,column=5)
+
+button = Button ( window,text="Submit", command=openGraph).grid(row=0,column=7)
+
+value_inside = StringVar(window)
+  
+# Set the default value of the variable
+value_inside.set("in")
+options = ["in", "ft", "m", "mm"]
+
+drop = OptionMenu(window, value_inside, *options).grid(row=2,column=2)
+
+def task():
+    
+    #print(graph1.outputs)
     window.after(1000, task)  # reschedule event in 2 seconds
 
 window.after(2000, task)
